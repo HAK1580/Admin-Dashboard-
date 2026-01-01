86205d337250dd8523d1221b89889aac4b463376
HAK1580 <aliofficial1580630@gmail.com>
2026-01-01 14:48:00 +0500

    dashboard is working fine

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-
+import { Line } from 'react-chartjs-2'
 const Analytics = () => {
+  const data={
+    labels:["jan","feb","march","apr","aug"],
+    datasets:[
+      {
+      label:"loss",
+      data:[100,200,300,400],
+      tension:0.5,
+      borderWidth:2
+      },
+      {
+      label:"progress",
+      data:[50,22,330,440],
+      tension:0.5,
+      borderWidth:2
+      },
+    ]
+  };
+  
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,   // <-- important
+  };
+
   return (
      <div className='sm:w-[85%] h-screen lg:w-[95%]' >
       <h1 className='text-3xl px-6 sm:py-5  lg:hidden ' >Analytics  </h1>
             <div className="add-content-here p-4">
         <h1 className="text-lg text-green-500 text-center">YOUR CONTENT HERE </h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit natus perspiciatis reprehenderit laborum. Assumenda voluptatum odio, blanditiis mollitia, maiores molestias reprehenderit error ratione debitis, praesentium beatae ut! Accusamus beatae, nostrum dolorum quidem vitae numquam ab dolorem adipisci? Expedita eaque dolor neque corporis sint accusamus nobis laboriosam provident laudantium alias id necessitatibus consequuntur recusandae, ipsum dolore minima, ullam aspernatur reiciendis. Eveniet sunt esse corporis aspernatur repellat excepturi itaque saepe ea et amet reiciendis animi, doloremque minus repudiandae incidunt quis dolorum vel inventore molestiae! Commodi velit quidem ratione! Voluptas, illum placeat dicta atque sequi numquam tenetur beatae molestiae ducimus nam consequatur quae.
-      </div>
+ Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita autem ratione aliquam voluptas reprehenderit maiores. Expedita nemo nulla voluptatibus explicabo in quidem sit labore id amet consequuntur voluptas, similique laborum asperiores nihil quisquam neque assumenda fugit ducimus animi? Eos molestiae ad ab est atque optio consequuntur quis voluptatem iure? Quam!
+       </div>
+       <div className="chart h-60  bg-gray-200">
+       <Line data={data} options={options    } />
+
+       </div>
+       
     </div>
   )
 }
